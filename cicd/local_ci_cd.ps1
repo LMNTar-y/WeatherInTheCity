59fe18c5d313df60e894ce2184e405172d4f7b9f
Aliaksei Balvanovich <a.balvanovich@gmail.com>
2022-06-24 12:15:00 +0300

    added publish instruction to the cicd and ignore result file and logs folder

--- cicd/local_ci_cd.ps1
+++ cicd/local_ci_cd.ps1
@@ -1,2 +1,9 @@
+Write-Host "Building is starting" -ForegroundColor red
 dotnet build ..\src\WeatherApp\
+Write-Host "Building complited" -ForegroundColor green
+Write-Host "Testing is starting" -ForegroundColor red
 dotnet test ..\src\WeatherApp\
+Write-Host "Testing finished" -ForegroundColor green
+Write-Host "Publishing is starting" -ForegroundColor red
+dotnet publish ..\src\WeatherApp\ -o ..\publish
+Write-Host "Publishing complited" -ForegroundColor green
